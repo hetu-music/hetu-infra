2330101a5720fe19bbd2ff4b1c4c3a5362991016
hetu-music <216563469+hetu-music@users.noreply.github.com>
2026-09-11 01:09:04 +0800

    fix

--- modules/cloudflare/dns/main.tf
+++ modules/cloudflare/dns/main.tf
@@ -9,4 +9,5 @@ resource "cloudflare_dns_record" "dns" {
   proxied  = each.value.proxied
   comment  = each.value.comment == "" ? null : each.value.comment
   priority = each.value.priority
+  tags     = distinct(concat(var.tags, each.value.tags))
 }
--- modules/cloudflare/dns/variables.tf
+++ modules/cloudflare/dns/variables.tf
@@ -3,16 +3,23 @@ variable "zone_id" {
   type        = string
 }
 
+variable "tags" {
+  description = "Default tags applied to all DNS records in this module"
+  type        = list(string)
+  default     = []
+}
+
 variable "records" {
   description = "A map of DNS records for this specific zone"
   type = map(object({
-    name     = string               # e.g., "www", "@", etc.
-    type     = string               # A, AAAA, CNAME, TXT, MX, NS, etc.
-    content  = string               # Target value (IP, alias, text, etc.)
-    ttl      = optional(number, 1)  # TTL (1 = automatic/auto in Cloudflare)
-    proxied  = optional(bool, true) # Whether to proxy traffic through Cloudflare
-    comment  = optional(string)     # Optional description for metadata
-    priority = optional(number)     # Optional priority for MX records
+    name     = string                     # e.g., "www", "@", etc.
+    type     = string                     # A, AAAA, CNAME, TXT, MX, NS, etc.
+    content  = string                     # Target value (IP, alias, text, etc.)
+    ttl      = optional(number, 1)        # TTL (1 = automatic/auto in Cloudflare)
+    proxied  = optional(bool, true)       # Whether to proxy traffic through Cloudflare
+    comment  = optional(string)           # Optional description for metadata
+    priority = optional(number)           # Optional priority for MX records
+    tags     = optional(list(string), []) # Tags for this specific DNS record
   }))
   default = {}
 }
